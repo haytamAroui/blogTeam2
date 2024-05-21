958ef5af4b350b107af96396dfd2dc0078f02e22
Haytam Aroui <haytam.aroui@gmail.com>
2024-05-21 11:54:27 +0200

    new

---
+++ src/main/java/be/intecbrussel/blogteam2/BlogTeam2Application.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.blogteam2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogTeam2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogTeam2Application.class, args);
+    }
+
+}
---
+++ src/test/java/be/intecbrussel/blogteam2/BlogTeam2ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.blogteam2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogTeam2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
