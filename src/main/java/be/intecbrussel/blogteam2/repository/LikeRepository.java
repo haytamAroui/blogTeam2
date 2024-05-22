371db73c22a1c3218b2e72335120e1c66cc20424
Deepika Aggarwal <145540773+aggdeepika26@users.noreply.github.com>
2024-05-22 14:45:17 +0200

    Delete src/main/java/be/intecbrussel/blogteam2/repository directory

--- src/main/java/be/intecbrussel/blogteam2/repository/LikeRepository.java
+++
@@ -1,7 +0,0 @@
-package be.intecbrussel.blogteam2.repository;
-
-import be.intecbrussel.blogteam2.models.Like;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LikeRepository extends JpaRepository<Like,Long> {
-}
--- src/main/java/be/intecbrussel/blogteam2/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package be.intecbrussel.blogteam2.repository;
-
-import be.intecbrussel.blogteam2.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
