727468b4b39d424401de3918c598f13bba5ef6f6
adeepika26@yahoo.in <adeepika26@yahoo.in>
2024-05-24 18:16:18 +0200

    checked on postman with all error msg

---
+++ src/main/java/be/intecbrussel/blogteam2/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package be.intecbrussel.blogteam2.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}
