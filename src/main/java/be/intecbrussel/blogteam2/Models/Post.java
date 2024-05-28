164cac54d03e31030f2d80b0b62da377d93c03ee
Haytam Aroui <haytam.aroui@gmail.com>
2024-05-28 13:58:43 +0200

    Update Post.java

--- src/main/java/be/intecbrussel/blogteam2/Models/Post.java
+++ src/main/java/be/intecbrussel/blogteam2/Models/Post.java
@@ -1,4 +1,63 @@
-package be.intecbrussel.blogteam2.Models;
+package be.intecbrussel.blogteam2.models;
 
-public class Post {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+import java.util.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@AllArgsConstructor
+@Setter
+@Getter
+@NoArgsConstructor
+@Table(name = "Posts")
+public class Post extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @Size(max = 100)
+    @Column(unique = true)
+    private String title;
+
+    @NotNull
+    @Size(max = 250)
+    private String description;
+    @NotNull
+
+    @Lob
+    private String content;
+    private String imageURL;
+
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "post")
+    private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Like> likes = new HashSet<>();
+
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    // @JsonIgnore
+    private User user;
+    // Transient field to hold userName
+
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "post_tags",
+            joinColumns = @JoinColumn(name = "post_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private Set<Tag> tags = new HashSet<>();
 }
