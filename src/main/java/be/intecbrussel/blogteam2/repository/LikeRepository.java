0002b9323aaf4ad4cb05992ff0c9a1ab42c32ef4
adeepika26@yahoo.in <adeepika26@yahoo.in>
2024-05-28 13:46:09 +0200

    LIKE

--- src/main/java/be/intecbrussel/blogteam2/repository/LikeRepository.java
+++ src/main/java/be/intecbrussel/blogteam2/repository/LikeRepository.java
@@ -4,6 +4,13 @@ import be.intecbrussel.blogteam2.models.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
+    boolean existsByUserIdAndPostId(Long userId, Long postId);
+    Long countByPostId(Long postId);
+
+    List<Like> findAllByPostIdAndUserId(Long postId, Long userId);
 }
