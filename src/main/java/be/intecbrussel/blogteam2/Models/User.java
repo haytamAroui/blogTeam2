b54ac59912f110f41183026902dcaa81e36c2d79
Haytam Aroui <haytam.aroui@gmail.com>
2024-05-28 13:58:20 +0200

    Update User.java

--- src/main/java/be/intecbrussel/blogteam2/Models/User.java
+++ src/main/java/be/intecbrussel/blogteam2/Models/User.java
@@ -1,4 +1,69 @@
-package be.intecbrussel.blogteam2.Models;
+package be.intecbrussel.blogteam2.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@AllArgsConstructor
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "Users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(nullable = false, unique = true)
+    private String userName;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String street;
+    private String houseNo;
+    private String city;
+    private String zip;
+
+    @NotNull
+    private String password;
+
+    @Transient
+    private String retypePassword;
+
+    private String avatar;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Post> posts = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Comment> comments = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> roles = new HashSet<>();
+
+    @PrePersist
+    protected void onCreate() {
+        if (roles == null || roles.isEmpty()) {
+            roles.add("ROLE_USER");
+        }
+    }
 }
